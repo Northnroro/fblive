b8357590c8658c668817426f6a37d58ca6854297
NORTHNRORO\Northnroro <northnroro@hotmail.com>
2020-08-12 00:28:22 +0800

    v1.1.1

---
+++ precache-manifest.f34c98ef37610cb8f228bab3a4361f83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3d95da3b3be3ffff6d1a78f569cecd",
+    "url": "/ffbookerlive/index.html"
+  },
+  {
+    "revision": "60779c1bc309fbffd6ec",
+    "url": "/ffbookerlive/static/css/main.76bc0bf6.chunk.css"
+  },
+  {
+    "revision": "e5d1f39d2d701851c924",
+    "url": "/ffbookerlive/static/js/2.1dba66cf.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/ffbookerlive/static/js/2.1dba66cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60779c1bc309fbffd6ec",
+    "url": "/ffbookerlive/static/js/main.18f6215f.chunk.js"
+  },
+  {
+    "revision": "95bba685ba5ecbf5305d",
+    "url": "/ffbookerlive/static/js/runtime-main.eb20fc29.js"
+  }
+]);
