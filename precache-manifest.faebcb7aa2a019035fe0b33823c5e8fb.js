a775f9c45cacccd60944f90cbfed53c4f34b790e
NORTHNRORO\Northnroro <northnroro@hotmail.com>
2020-08-12 00:19:48 +0800

    v1.1.0

---
+++ precache-manifest.faebcb7aa2a019035fe0b33823c5e8fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7654487ef6d643205ddec2fbf5147f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e3535464518a2a3dbf",
+    "url": "/static/css/main.76bc0bf6.chunk.css"
+  },
+  {
+    "revision": "e5d1f39d2d701851c924",
+    "url": "/static/js/2.1dba66cf.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/static/js/2.1dba66cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e3535464518a2a3dbf",
+    "url": "/static/js/main.f9e91756.chunk.js"
+  },
+  {
+    "revision": "3e0c3b2aac9b75c1c832",
+    "url": "/static/js/runtime-main.5bc8739a.js"
+  }
+]);
